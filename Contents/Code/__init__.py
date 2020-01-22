32f3aaa0405f71e453532a4387af525639da2de3
psychoria <psychoria@hotmail.com>
2020-01-21 23:08:06 -0400

    Json metadata can be used for detail information

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -2,7 +2,8 @@
 
 from .movie_list import search_naver_movie
 from .movie_detail import update_naver_movie
-from .metadata_parser import get_metadata_path, parse_metadata
+from .metadata_parser import is_search_metadata_available, parse_search_metadata, \
+    is_detail_metadata_available, parse_detail_metadata
 
 
 def Start():
@@ -18,11 +19,15 @@ class NaverMovieAgent(Agent.Movies):
     accepts_from = ['com.plexapp.agents.localmedia']
 
     def search(self, results, media, lang):
-        metadata_path = get_metadata_path(media=media)
-        if metadata_path is not None:
-            return parse_metadata(results, lang, metadata_path)
+        metadata_exists = is_search_metadata_available(media=media)
+        if metadata_exists:
+            return parse_search_metadata(media=media, lang=lang, results=results)
         else:
             return search_naver_movie(results, media, lang)
 
     def update(self, metadata, media, lang):
-        update_naver_movie(metadata, media, lang)
+        metadata_exists = is_detail_metadata_available(media=media)
+        if metadata_exists:
+            parse_detail_metadata(media=media, metadata=metadata)
+        else:
+            update_naver_movie(metadata, media, lang)
--- Contents/Code/metadata_parser.py
+++ Contents/Code/metadata_parser.py
@@ -2,11 +2,12 @@
 
 import json
 import os
+import unicodedata
 import urllib
 from os.path import dirname, exists, join
 
 
-def get_metadata_path(media):
+def get_search_metadata_path(media):
     metadata_path = join(dirname(urllib.unquote(media.filename).decode('utf-8')), 'metadata.json')
     if exists(metadata_path):
         return metadata_path
@@ -14,10 +15,104 @@ def get_metadata_path(media):
         return None
 
 
-def parse_metadata(results, lang, metadata_path):
-    fd = os.open(metadata_path, os.O_RDONLY)
-    f = os.fdopen(fd, 'rt')
-    metadata = json.load(f)
-    movie_id, title, year, score = metadata['id'], metadata['title'], metadata['year'], 100
-    Log.Debug('From JSON metadata id: %s, title: %s, year: %s' % (movie_id, title, year))
-    results.Append(MetadataSearchResult(id=movie_id, name=title, year=year, score=score, lang=lang))
+def is_search_metadata_available(media):
+    return True if get_search_metadata_path(media) is not None else False
+
+
+def parse_search_metadata(media, lang, results):
+    metadata_path = join(dirname(urllib.unquote(media.filename).decode('utf-8')), 'metadata.json')
+    with os.fdopen(os.open(metadata_path, os.O_RDONLY), 'rt') as f:
+        json_data = json.load(f)
+        movie_id, title, year, score = json_data['id'], json_data['title'], json_data['year'], 100
+        Log.Debug('From JSON metadata id: %s, title: %s, year: %s' % (movie_id, title, year))
+        results.Append(MetadataSearchResult(id=movie_id, name=title, year=year, score=score, lang=lang))
+
+
+def get_detail_metadata_path(media):
+    metadata_path = join(dirname(urllib.unquote(media.items[0].parts[0].file).decode('utf-8')), 'metadata.json')
+    if exists(metadata_path):
+        return metadata_path
+    else:
+        return None
+
+
+def is_detail_metadata_available(media):
+    metadata_path = get_detail_metadata_path(media=media)
+
+    if exists(metadata_path):
+        with os.fdopen(os.open(metadata_path, os.O_RDONLY), 'rt') as f:
+            json_data = json.load(f)
+            if 'detail' in json_data:
+                return True
+            else:
+                return False
+    return False
+
+
+def parse_detail_metadata(media, metadata):
+    metadata_path = get_detail_metadata_path(media)
+    with os.fdopen(os.open(metadata_path, os.O_RDONLY), 'rt') as f:
+        json_data = json.load(f)
+        detail = json_data['detail']
+
+        metadata.title = media.title
+        metadata.year = int(json_data['year'])
+        metadata.title_sort = unicodedata.normalize('NFKD', metadata.title[0])[0] + ' ' + metadata.title
+
+        # Basic Information
+        if 'original_title' in detail:
+            metadata.original_title = detail['original_title']
+        if 'rating' in detail:
+            metadata.rating = float(detail['rating'])
+        if 'originally_available_at' in detail:
+            metadata.originally_available_at = Datetime.ParseDate(detail['originally_available_at']).date()
+        if 'content_rating' in detail:
+            metadata.content_rating = detail['content_rating']
+        if 'summary' in detail:
+            metadata.summary = detail['summary']
+
+        info_types = [['genres', metadata.genres], ['countries', metadata.countries]]
+        for info_type in info_types:
+            if info_type[0] in detail:
+                [info_type[1].add(info) for info in detail[info_type[0]]]
+
+        # Actors
+        metadata.roles.clear()
+        if 'roles' in detail:
+            for info in detail['roles']:
+                actor = metadata.roles.new()
+                actor.name = info['name'] if 'name' in info else None
+                actor.photo = info['photo'] if 'photo' in info else None
+                actor.role = info['role'] if 'role' in info else None
+
+        # Directors & Producers & Writers
+        person_types = [['directors', metadata.directors], ['producers', metadata.producers],
+                        ['writers', metadata.writers]]
+
+        for person_type in person_types:
+            if person_type[0] in detail:
+                person_type[1].clear()
+                for person in detail[person_type[0]]:
+                    new_person = person_type[1].new()
+                    new_person.name = person['name']
+                    new_person.photo = person['photo']
+
+        # Post & Art
+        if 'photos' in detail:
+            photo_types = [['posters', Prefs['max_num_posters'], metadata.posters],
+                           ['arts', Prefs['max_num_arts'], metadata.art]]
+            photos = detail['photos']
+            for photo_type in photo_types:
+                if photo_type[0] in photos:
+                    for photo_url in photos[photo_type[0]]:
+                        index = 0
+                        try:
+                            index += 1
+                            url = photo_url
+                            photo_type[2][url] = Proxy.Preview(HTTP.Request(url), sort_order=index)
+                            if photo_type[1] <= index:
+                                break
+                        except Exception:
+                            Log.Error('Error with photo information: %s (Index: %d)' % (metadata.title, index))
+
+        Log.Debug('Metadata for %s is parsed from JSON' % metadata.title)
