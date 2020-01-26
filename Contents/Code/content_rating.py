6a1ffd350aab1b7d8a8e0f666980e19bf90e65d5
psychoria <psychoria@hotmail.com>
2020-01-25 23:06:18 -0400

    fixed unicode issue

--- Contents/Code/content_rating.py
+++ Contents/Code/content_rating.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 
 content_ratings = {
-    '전체 관람가': {'KMRB': '전체 관람가', 'MPAA': 'G'},
-    '12세 관람가': {'KMRB': '12세 관람가', 'MPAA': 'PG'},
-    '15세 관람가': {'KMRB': '15세 관람가', 'MPAA': 'PG-13'},
-    '청소년 관람불가': {'KMRB': '청소년 관람불가', 'MPAA': 'R'},
-    '제한상영가': {'KMRB': '청소년 관람불가', 'MPAA': 'NC-17'},
-    '등급보류': {'KMRB': '등급보류', 'MPAA': 'NR'},
-    'G': {'KMRB': '전체 관람가', 'MPAA': 'G'},
-    'PG': {'KMRB': '12세 관람가', 'MPAA': 'PG'},
-    'PG-13': {'KMRB': '15세 관람가', 'MPAA': 'PG-13'},
-    'R': {'KMRB': '청소년 관람불가', 'MPAA': 'R'},
-    'NC-17': {'KMRB': '청소년 관람불가', 'MPAA': 'NC-17'},
-    'X[NC-17]': {'KMRB': '청소년 관람불가', 'MPAA': 'NC-17'},
-    'NR': {'KMRB': '등급보류', 'MPAA': 'NR'},
+    u'전체 관람가': {u'KMRB': u'전체 관람가', u'MPAA': u'G'},
+    u'12세 관람가': {u'KMRB': u'12세 관람가', u'MPAA': u'PG'},
+    u'15세 관람가': {u'KMRB': u'15세 관람가', u'MPAA': u'PG-13'},
+    u'청소년 관람불가': {u'KMRB': u'청소년 관람불가', u'MPAA': u'R'},
+    u'제한상영가': {u'KMRB': u'청소년 관람불가', u'MPAA': u'NC-17'},
+    u'등급보류': {u'KMRB': u'등급보류', u'MPAA': u'NR'},
+    u'G': {u'KMRB': u'전체 관람가', u'MPAA': u'G'},
+    u'PG': {u'KMRB': u'12세 관람가', u'MPAA': u'PG'},
+    u'PG-13': {u'KMRB': u'15세 관람가', u'MPAA': u'PG-13'},
+    u'R': {u'KMRB': u'청소년 관람불가', u'MPAA': u'R'},
+    u'NC-17': {u'KMRB': u'청소년 관람불가', u'MPAA': u'NC-17'},
+    u'X[NC-17]': {u'KMRB': u'청소년 관람불가', u'MPAA': u'NC-17'},
+    u'NR': {u'KMRB': u'등급보류', u'MPAA': u'NR'},
 }
 
 
